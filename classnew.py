2bb951fdf808d2d0d3357e09f4176c034b849ff6
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-16 21:52:49 +0530

    Add files via upload

---
+++ classnew.py
@@ -0,0 +1,33 @@
+class myclass:
+  x = 10
+
+obj1 = myclass()
+print(obj1.x)
+
+# O/P - 10
+ 
+ 
+class sa:
+  name = "HELLO SAJIN"
+
+obj2 = sa()
+print(obj2.name)
+
+#O/P - HELLO SAJIN
+
+
+class nijas:
+  def __init__(self,name,age):
+    self.name = name
+    self.age = age
+  
+objj = nijas("SAJIN",21)
+
+print(objj.name)
+print(objj.age)
+
+#O/P 
+'''
+SAJIN
+21
+'''
